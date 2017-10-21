3293e7e39d1b2314dcde4ffdb8b51afcac6086db
websubrat <web.subrat@gmail.com>
2017-10-21 16:45:47 +0530

    initil commit

---
+++ storekeeper/src/main/java/com/api/salesmagnet/storekeeper/StorekeeperApplication.java
@@ -0,0 +1,12 @@
+package com.api.salesmagnet.storekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorekeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorekeeperApplication.class, args);
+	}
+}
