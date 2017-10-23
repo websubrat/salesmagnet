b12f65931e11db74ac39661c154e8d32a5560b3f
websubrat <web.subrat@gmail.com>
2017-10-23 19:12:44 +0530

    adding zull proxy

--- salesbook/src/main/java/com/api/salesmagnet/salesbook/SalesbookApplication.java
+++ salesbook/src/main/java/com/api/salesmagnet/salesbook/SalesbookApplication.java
@@ -2,10 +2,12 @@ package com.api.salesmagnet.salesbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @RestController
 public class SalesbookApplication {
 	
