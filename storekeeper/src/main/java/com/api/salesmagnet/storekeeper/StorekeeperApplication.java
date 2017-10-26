4ec0e45beec5682f1d2d11c363bd8824e0299fbd
websubrat <web.subrat@gmail.com>
2017-10-26 21:09:38 +0530

    adding config server

--- storekeeper/src/main/java/com/api/salesmagnet/storekeeper/StorekeeperApplication.java
+++ storekeeper/src/main/java/com/api/salesmagnet/storekeeper/StorekeeperApplication.java
@@ -1,19 +1,26 @@
 package com.api.salesmagnet.storekeeper;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@RefreshScope
+@RequestMapping("/storekeeper")
 public class StorekeeperApplication {
 	
-	@RequestMapping("storekeeper/hello")
+	@Value("$(message: Default hello)")
+	private String message;
+	
+	@RequestMapping("/hello")
     String hello() {
-        return "Hello Storekeeper!!";
+        return "Hello Storekeeper!! says"+message;
     }
 
 	public static void main(String[] args) {
